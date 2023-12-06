60be5680979eab1376a587a62bfaf6b94ac1c4f5
黃河濤 <3B032081@gm.student.ncut.edu.tw>
2023-12-06 10:43:45 +0800

    bb.b1

--- routes/web.php
+++ routes/web.php
@@ -27,5 +27,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
+Route::get('/bb/b1', function () {
+    return view('bb.b1');
+});
 require __DIR__.'/auth.php';

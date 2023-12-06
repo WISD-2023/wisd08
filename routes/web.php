211b5e82d7b27d0324f889fd85651df16cb7e3e8
鄭哲丞 <a0968518713@gmail.com>
2023-12-06 11:29:32 +0800

    cc.c2

--- routes/web.php
+++ routes/web.php
@@ -47,4 +47,7 @@ Route::get('/bb/b2', function () {
 Route::get('/cc/c1', function () {
     return view('cc.c1');
 });
+Route::get('/cc/c2', function () {
+    return view('cc.2');
+});
 require __DIR__.'/auth.php';
